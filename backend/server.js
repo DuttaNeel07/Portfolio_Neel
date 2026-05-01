bfdb8357f124ffbcd75d13714a8db4b86576bff8
Neel Dutta <neel.dutta0729@gmail.com>
2026-05-01 10:14:39 +0530

    add error message

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db.js');
-const port = process.env.PORT || 10000;
 const contactRoutes = require('./routes/contactRoutes');
 const projectRoutes = require('./routes/projectRoutes');
-
-connectDB();
-
 const cors = require('cors');
+
+const port = process.env.PORT || 10000;
+
 app.use(express.json());
 app.use(cors());
+
 app.use('/', contactRoutes);
 app.use('/', projectRoutes);
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+
+  } catch (error) {
+    console.error("Startup Error:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
